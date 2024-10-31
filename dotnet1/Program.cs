3dcc52af579d2a2846886a8ac147c47c05aab4d7
Oleksii Korniienko <kornienko1296@gmail.com>
2024-10-31 11:31:33 +0200

    Add more dashboards

--- dotnet1/Program.cs
+++ dotnet1/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 Console.WriteLine("OTEL_EXPORTER_OTLP_ENDPOINT = " + builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
 
 // Add services to the container.
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddOpenTelemetry()
     .WithMetrics(metrics =>
@@ -17,32 +18,52 @@ builder.Services.AddOpenTelemetry()
         metrics.AddMeter("Microsoft.AspNetCore.Hosting");
         metrics.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
 
-       // metrics.AddOtlpExporter();
+        metrics.AddOtlpExporter();
         metrics.AddPrometheusExporter();
     });
 
 var app = builder.Build();
+
 app.MapPrometheusScrapingEndpoint();
 
 
-var sampleTodos = new Todo[]
-{
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
 
 var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () =>
-{
-    return sampleTodos;
-});
-todosApi.MapGet("/{id:int}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+todosApi.MapGet("/", TodoGenerator.GenerateRandomTodo);
+
 app.Run();
 
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+public record Todo(Guid Id, string? Title, string Description, DateOnly? DueBy = null, bool IsComplete = false);
+
+
+public static class TodoGenerator
+{
+    private static readonly Random Random = new Random(200);
+    private static readonly string[] Titles = { "Buy groceries", "Complete project report", "Call a friend", "Read a book", "Exercise", "Plan the weekend" };
+
+    public static Todo GenerateRandomTodo()
+    {
+        var id = Guid.NewGuid();
+        var title = Titles[Random.Next(Titles.Length)];
+        var dueBy = DateOnly.FromDateTime(DateTime.Today.AddDays(Random.Next(1, 30))); // Random due date within the next 30 days
+        var isComplete = Random.Next(2) == 0; // Randomly assigns true or false
+        var descriptionLength = Random.Next(100, 1000);
+        var description = GenerateRandomString(descriptionLength);
+        
+        return new Todo(id, title, description, dueBy, isComplete);
+    }
+    
+    static string GenerateRandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        char[] result = new char[length];
+    
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = chars[Random.Shared.Next(chars.Length)];
+        }
+
+        return new string(result);
+    }
+}
+
--- k6-scripts/k6.js
+++ k6-scripts/k6.js
@@ -4,10 +4,11 @@ import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
     stages: [
-        { duration: '30s', target: 50 },
-         { duration: '2m', target: 2000 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '30s', target: 5 },
+         { duration: '1m', target: 1000 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
         { duration: '1m', target: 0 }, // ramp-down to 0 users
-        { duration: '4m', target: 10 }, // ramp-down to 0 users 
+        { duration: '10m', target: 20000 }, 
+        { duration: '1m', target: 0 }
     ],
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
