15f7db4fa12052ec0a2bae5b1db8cf65d47cc984
Oleksii Korniienko <kornienko1296@gmail.com>
2024-11-01 21:29:01 +0200

    Added healthcheck modules

---
+++ WebAppAot/WebAotSample/HealthCheckModule.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using HealthCheck.ModuleInitializer;
+
+namespace WebAotSample;
+
+public static class HealthCheckModule
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        HealthCheckInitializer.Initialize();
+    }
+}
---
+++ WebAppNoAot/HealthCheckModule.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using HealthCheck.ModuleInitializer;
+
+namespace WebAppNoAot;
+
+public static class HealthCheckModule
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        HealthCheckInitializer.Initialize();
+    }
+}
